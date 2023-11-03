31382c4397f5210f9d550884f15e8462ab166fe6
ausprouty <bob.prouty@powertochange.org.au>
2023-11-03 18:32:46 +1100

    video now changes upon change of segment

--- src/components/JesusVideo/JVideoPlayer.vue
+++ src/components/JesusVideo/JVideoPlayer.vue
@@ -6,37 +6,68 @@
 import { api } from "boot/axios";
 
 export default {
-  name: 'JVideoPlayer',
-  props:{
+  name: "JVideoPlayer",
+  props: {
     videoSegment: String,
     languageCodeHL: String,
   },
   data() {
     return {
-      show1 : false,
-      iframeStart: '<iframe src="https://api.arclight.org/videoPlayerUrl?refId=',
-      iframeEnd: '&playerStyle=default" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>',
+      show1: false,
+      iframeStart:
+        '<iframe src="https://api.arclight.org/videoPlayerUrl?refId=',
+      iframeEnd:
+        '&playerStyle=default" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>',
       videoIframe: null,
     };
   },
 
-  created(){
-    showVideo()
+  created() {
+    this.updateVideoIframe(this.languageCodeHL, this.videoSegment)
   },
-  methods:{
-    showVideo(){
-      var url =  'api/video/code/JESUS/' + this.languageCodeHL
-    api.get(url).then((response) => {
-      var video1 = response.data.replace('-0-0', this.videoSegment)
-      this.videoIframe = this.iframeStart + video1 + this.iframeEnd
-    });
-
-    }
-  }
-}
+  watch: {
+    languageCodeHL: function (newLanguage, oldLanguage) {
+      if (newLanguage !== oldLanguage) {
+        this.updateVideoIframe(newLanguage, this.videoSegment);
+      }
+    },
+    videoSegment: function (newVideoSegment, oldVideoSegment) {
+      if (newVideoSegment !== oldVideoSegment) {
+        this.updateVideoIframe(this.languageCodeHL, newVideoSegment);
+      }
+    },
+  },
+  methods: {
+    updateVideoIframe(languageCodeHL, videoSegment) {
+      var url = "api/video/code/JESUS/" + languageCodeHL;
+      api.get(url).then((response) => {
+        var video1 = response.data.replace("-0-0", videoSegment);
+        this.videoIframe = this.iframeStart + video1 + this.iframeEnd;
+      });
+    },
+  },
+};
 </script>
 <style>
-  .arc-cont{position:relative;display:block;margin:10px auto;width:100%}
-  .arc-cont:after{padding-top:59%;display:block;content:""}
-  .arc-cont>iframe{position:absolute;top:0;bottom:0;right:0;left:0;width:98%;height:98%;border:0}
+.arc-cont {
+  position: relative;
+  display: block;
+  margin: 10px auto;
+  width: 100%;
+}
+.arc-cont:after {
+  padding-top: 59%;
+  display: block;
+  content: "";
+}
+.arc-cont > iframe {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 98%;
+  height: 98%;
+  border: 0;
+}
 </style>
