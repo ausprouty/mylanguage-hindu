07a80d275c57778a0ea8d449e6b686b94094c8d0
ausprouty <bob.prouty@powertochange.org.au>
2023-11-23 11:23:48 +1100

    About to try new table idea

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,22 +2,24 @@
   <q-page class="bg-white" padding>
     <p>In order to make any real progress in our spiritual life we know we need a guru -- someone to show us the way, to challenge, correct, guide and help us.</p>
     <p> We would like to invite you to consider having Lord Jesus as your Guru</p>
-      <p> Lord Jesus:</p>
-      <ul>
-        <li> Never told a lie</li>
-        <li>Etc</li>
-      </ul>
-    <q-list bordered class="list">
-      <q-row>
-        <q-col v-for="(item, index) in menuItems" :key="index" cols="6">
-          <q-item :to="item.to" clickable v-ripple>
-            <q-item-section class="menu_item">
-              <img class="menu_picture" :src="item.imageSrc">
-            </q-item-section>
-          </q-item>
+    <p> Lord Jesus:</p>
+    <ul>
+      <li> Never told a lie</li>
+      <li>Etc</li>
+    </ul>
+    <q-table bordered class="list">
+      <q-row v-for="(row, rowIndex) in Math.ceil(menuItems.length / 2)" :key="rowIndex">
+        <q-col class="side-by-side" v-for="colIndex in 2" :key="colIndex" cols="2">
+          <template v-if="menuItems[(rowIndex * 2) + colIndex]">
+            <q-item :to="menuItems[(rowIndex * 2) + colIndex].to" clickable v-ripple>
+              <q-item-section class="menu_item">
+                <img class="menu_picture" :src="menuItems[(rowIndex * 2) + colIndex].imageSrc">
+              </q-item-section>
+            </q-item>
+          </template>
         </q-col>
       </q-row>
-    </q-list>
+    </q-table>
   </q-page>
 </template>
 
@@ -48,18 +50,21 @@ export default defineComponent({
 .menu_item {
   margin-left: 10px;
   margin-right: 10px;
-  max-width: 580px;
-  width: calc(45% - 20px);
+  max-width: 200px;
+  width: calc(100% - 20px);
   margin: 0 auto;
   background-color: #f0f0f0; /* Just for visibility */
   padding: 10px; /* Optional: Add padding for content within the element */
 }
 .menu_picture {
-  max-width: 580px;
-  width: calc(45% - 0px);
+  max-width: 200px;
+  width: calc(100% - 0px);
   margin: 0 auto;
   background-color: #f0f0f0; /* Just for visibility */
   padding: 10px; /* Optional: Add padding for content within the element */
+}
+q-col.side-by-side{
+  width:20px;
 }
 .xlist{
   width:100%;
