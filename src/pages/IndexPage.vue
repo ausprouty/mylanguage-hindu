aadf422987cb9cb75a5716e1655a83758fa4810e
ausprouty <bob.prouty@powertochange.org.au>
2023-11-23 13:26:42 +1100

    Index Page Works

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,39 +7,40 @@
       <li> Never told a Lie</li>
       <li>Etc</li>
     </ul>
-  
-    <q-row>
-      <q-col class="side-by-side" to="/mountain" clickable v-ripple >
+    <table>
+    <tr class = "full-width">
+      <td class="side-by-side" @click="handleImageClick('/mountain')" clickable v-ripple >
         <img  class="menu_picture" src="menu/mountain.png">
-      </q-col>
-      <q-col class="side-by-side" to="/birth" clickable v-ripple>
-        <img  class="menu_picture" src="menu/birth.png">
-       </q-col>
-    </q-row>
-    <q-row>
-      <q-col class="side-by-side" to="/life" clickable v-ripple >
+      </td>
+      <td class="side-by-side"  @click="handleImageClick('/birth')" clickable v-ripple>
+        <img   class="menu_picture" src="menu/birth.png">
+      </td>
+    </tr>
+    <tr class = "full-width">
+      <td class="side-by-side" @click="handleImageClick('/life')" clickable v-ripple >
         <img  class="menu_picture" src="menu/life.png">
-      </q-col>
-      <q-col class="side-by-side" to="/teachings" clickable v-ripple >
+      </td>
+      <td class="side-by-side" @click="handleImageClick('/teachings')" clickable v-ripple >
         <img  class="menu_picture" src="menu/teachings.png">
-      </q-col>
-    </q-row>
-    <q-row>
-      <q-col class="side-by-side" to="/book" clickable v-ripple  >
+      </td>
+    </tr>
+    <tr class = "full-width">
+      <td class="side-by-side" @click="handleImageClick('/book')" clickable v-ripple  >
         <img  class="menu_picture" src="menu/book.png">
-      </q-col>
-      <q-col class="side-by-side" to="/following" clickable v-ripple >
+      </td>
+      <td class="side-by-side" @click="handleImageClick('/following')" clickable v-ripple >
         <img  class="menu_picture" src="menu/following.png">
-      </q-col>
-    </q-row>
-    <q-row>
-      <q-col class="side-by-side" to="/leadership" clickable v-ripple >
+      </td>
+    </tr>
+    <tr class = "full-width">
+      <td class="side-by-side" @click="handleImageClick('/leadership')" clickable v-ripple >
         <img class="menu_picture" src="menu/leading.png">
-      </q-col>
-      <q-col class="side-by-side" to="/questions" clickable v-ripple >
+      </td>
+      <td class="side-by-side" @click="handleImageClick('/questions')" clickable v-ripple >
           <img class="menu_picture" src="menu/questions.png">
-      </q-col>
-    </q-row>
+      </td>
+    </tr>
+  </table>
   </q-page>
 </template>
 
@@ -51,6 +52,12 @@ export default{
       selected: null,
     };
   },
+  methods: {
+    handleImageClick(to) {
+      // Handle the click event, e.g., navigate to the specified route
+      this.$router.push(to);
+    },
+  }
 
 
 
@@ -68,16 +75,19 @@ export default{
   padding: 10px; /* Optional: Add padding for content within the element */
 }
 .menu_picture {
-  max-width: 250px;
-  width: calc(50% - 10px);
+  width: calc(100% - 10px);
   margin: 0 auto;
-  background-color: #f0f0f0; /* Just for visibility */
+  background-color: #eee; /* Just for visibility */
   padding: 10px; /* Optional: Add padding for content within the element */
 }
-q-col.side-by-side{
+td.side-by-side{
   width:45%;
-  padding: 10px;
+  padding: 0px;
 
+
+}
+tr.full-width{
+  width:100%;
 }
 
 </style>
